183ea9914e325206315dab72607dfc21fa928dbe
Edoardo Piergentili <epiergentili@outlook.it>
2020-07-06 18:38:35 +0200

    @3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,10 @@
+@section('page_title' , 'Welcome')
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>@yield('page_title')</title>
   </head>
   <body>
     <h1>HELLO</h1>
